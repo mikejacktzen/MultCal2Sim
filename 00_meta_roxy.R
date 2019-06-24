ea210db486c8a360c0e8aac696948b73b13f1f55
mikejacktzen <michaeltzen@g.ucla.edu>
2019-06-24 12:03:12 -0700

    setup project skeleton

---
+++ 00_meta_roxy.R
@@ -0,0 +1,15 @@
+
+datzen::rmabd()
+setwd("~/projects/MultCal2Sim/")
+devtools::load_all()
+devtools::document()
+roxygen2::roxygenise()
+
+
+
+# rd2md_all(dir_man_rd = "~/projects/MultCal2Sim/man/",
+#             dir_man_md = "~/projects/MultCal2Sim/man_md/")
+
+
+
+# devtools::use_readme_rmd()
